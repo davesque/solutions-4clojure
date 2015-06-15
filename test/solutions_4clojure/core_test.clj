b9a8d6e2edd709caad7ae0599b5bb14abfc42e3a
David Sanders <davesque@gmail.com>
2015-06-14 23:22:46 -0600

    Add tests for my-interleave

--- test/solutions_4clojure/core_test.clj
+++ test/solutions_4clojure/core_test.clj
@@ -2,6 +2,10 @@
   (:require [clojure.test :refer :all]
             [solutions-4clojure.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest my-interleave-test
+  (testing "It interleaves lists."
+    (is (= (my-interleave [1 2 3] [:a :b :c]) [1 :a 2 :b 3 :c])))
+  (testing "It correctly handles lists of differing length."
+    (is (= (my-interleave [1 2] [:a :b :c]) [1 :a 2 :b])))
+  (testing "It correctly handles empty lists."
+    (is (= (my-interleave [] [:a :b :c]) []))))
